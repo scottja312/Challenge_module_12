d9d21dbe72f4c72a1a23e82275eca92380c9f233
scottja312 <64980711+scottja312@users.noreply.github.com>
2020-09-13 00:08:39 -0400

    finished bar chart for dashboard.

--- plots.js
+++ plots.js
@@ -2,60 +2,76 @@
 function init() {
     var selector = d3.select("#selDataset");
   
-d3.json("samples.json").then((data) => {
-    console.log(data);
-    var sampleNames = data.names;
-    sampleNames.forEach((sample) => {
-    selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
+    d3.json("samples.json").then((data) => {
+        console.log(data);
+        var sampleNames = data.names;
+        sampleNames.forEach((sample) => {
+        selector
+            .append("option")
+            .text(sample)
+            .property("value", sample);
     });
-})}
-  
-init();
-
-// Adds "optionChanged function" to sync with obj created in index.html for a drop-down menu.
-function optionChanged(newSample) {
-    console.log(newSample);
+        var firstSample = sampleNames[0];
+        buildMetadata(firstSample);
+        buildCharts(firstSample);
+  });
 }
-
-// Print information to the Demographic Info panel when ID # is selected.
 
 // Step 1. Declare buildMetaData and buildCharts variables
 function optionChanged(newSample) {
     buildMetadata(newSample);
     buildCharts(newSample);
 }
+
+// Print information to the Demographic Info panel when ID # is selected.
 // Step 2. Declare buildMetadata() function.
 function buildMetadata(sample) {
     d3.json("samples.json").then((data) => {
       var metadata = data.metadata;
       var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
       var result = resultArray[0];
+      
       var PANEL = d3.select("#sample-metadata");
-  
       PANEL.html("");
       //PANEL.append("h6").text(result.location);
       // Skill Drill: modify buildMetadata() function to populate the 
      // Demographic Info panel with the rest of the demographic data.
-      Object.entries(result).forEach(([key, value]) => {PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+      Object.entries(result).forEach(([key, value]) => 
+      {PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
     });
-});
-}
-
+})}
+init();
 ////////////////////////
 // Challenge //
 
 // Declare buildCharts(sample) function to pull d3.json and samples data file.
-//function buildCharts(sample) {
-    //d3.json("samples.json").then((data) => {
-      //var samples = data.samples;
-      //var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
-      //var result = resultArray[0];
-      
-      //var PANEL = d3.select("#sample-metadata");
-  
-      //PANEL.html("");
+function buildCharts(sample) {
+    d3.json("samples.json").then((data) => {
+    var samples = data.samples;
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
 
-// Create a bar chart of the top ten bacterial species in a volunteer's navel.
+// Declare variables for bar chart.
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
+
+// Setup y-axis label from otu_ids 
+    var yaxis = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+
+// Create a Bar Chart with the sample arrays.
+    var trace = {
+        x: sample_values.slice(0, 10).reverse(),
+        y: yaxis,
+        text: otu_labels.slice(0, 10).reverse(),
+        type: "bar",
+        orientation: "h",
+};
+    var chart_data = [trace];
+    var chart_layout = {
+        title: "Top 10 OTUs",
+        margin: { t:50, l: 150 }
+};
+    Plotly.newPlot("bar", chart_data, chart_layout);
+    });
+}
